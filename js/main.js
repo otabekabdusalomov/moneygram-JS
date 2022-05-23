70229cc0fd9adeaa77d07075a15eef127dc7f02b
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2022-05-23 13:04:59 +0300

    if else

--- js/main.js
+++ js/main.js
@@ -8,38 +8,50 @@ const elBtnReset = document.querySelector(".btn__reset");
 
 
 elForm.addEventListener("submit", function(evt) {
-  evt.preventDefault()
-
-  elHeading.textContent = "Summangizni ko'rishingiz mumkin!";
-  elBtn.textContent = "Tugadi";
-  elBtnReset.textContent = "Qaytadan kiritish";
-
-  const dollar = 11092.50;
-  const euro = 11709.08;
-  const rubl = 178.91;
-  const funt = 13852.86;
-  const yuan = 1657.33;
-
-  const inputValue = elFormInput.value;
-  const selectorValue = elFormSelect.value;
-
-  if (selectorValue == "dollar") {
-    elFormGoal.textContent= `${dollar * inputValue} so'm'`;
-  } else if (selectorValue == "yevro") {
-    elFormGoal.textContent= `${euro * inputValue} so'm'`;
-  } else if (selectorValue == "rubl") {
-    elFormGoal.textContent = `${rubl * inputValue} so'm'`;
-  } else if (selectorValue == "funt") {
-    elFormGoal.textContent = `${funt * inputValue} so'm`;
-  } else if (selectorValue == "yuan") {
-    elFormGoal.textContent = `${yuan * inputValue} so'm`;
-  }
-
+    evt.preventDefault()
+    
+    elHeading.textContent = "Chorsu valyuta bozor";
+    elBtn.textContent = "Amalga oshirildi";
+    elBtnReset.textContent = "Takror urinish";
+    
+    const dollar = 11092.50;
+    const euro = 11709.08;
+    const rubl = 178.91;
+    const tilla = 21000.00;
+    const dinor = 24000.09;
+    const canada = 11215.00;
+    const shvetsariya = 11484.51;
+    const funt = 13852.86;
+    const yuan = 1657.33;
+    
+    const inputValue = elFormInput.value;
+    const selectorValue = elFormSelect.value;
+    
+    if (selectorValue == "dollar") {
+        elFormGoal.textContent= `${dollar * inputValue} so'm`;
+    } else if (selectorValue == "yevro") {
+        elFormGoal.textContent= `${euro * inputValue} so'm`;
+    } else if (selectorValue == "rubl") {
+        elFormGoal.textContent = `${rubl * inputValue} so'm`;
+    } else if (selectorValue == "tilla") {
+        elFormGoal.textContent = `${tilla * inputValue} so'm`;
+    } else if (selectorValue == "dinor") {
+        elFormGoal.textContent = `${dinor * inputValue} so'm`;
+    } else if (selectorValue == "canada") {
+        elFormGoal.textContent = `${canada * inputValue} so'm`;
+    } else if (selectorValue == "shvetsariya") {
+        elFormGoal.textContent = `${shvetsariya * inputValue} so'm`;
+    } else if (selectorValue == "funt") {
+        elFormGoal.textContent = `${funt * inputValue} so'm`;
+    } else if (selectorValue == "yuan") {
+        elFormGoal.textContent = `${yuan * inputValue} so'm`;
+    }
+    
 })
 
-elBtnReset.addEventListener("click", function() {
-  elHeading.textContent = "MONEYGRAM";
-  elBtn.textContent = "BOSHLASH";
-  elBtnReset.textContent = ".....";
-  elFormResult.textContent = "SUMMA"
-})
+// elBtnReset.addEventListener("click", function() {
+//   elHeading.textContent = "MONEYGRAM";
+//   elBtn.textContent = "BOSHLASH";
+//   elBtnReset.textContent = ".....";
+//   elFormResult.textContent = "SUMMA"
+// })
